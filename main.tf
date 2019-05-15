708c5413b2681f96e301b8d4e4ea57d997913429
edivet <35701536+edivet@users.noreply.github.com>
2019-05-16 00:03:02 +0200

    upload terraform

--- main.tf
+++ main.tf
@@ -1,24 +1,25 @@
 provider "scaleway" {
-  region = "ams1"
+  organization = "${var.organization}"
+  token        = "${var.token}"
+  region       = "${var.region}"
 }
 
-data "scaleway_image" "terraform" {
+data "scaleway_image" "wave_tf" {
   architecture = "x86_64"
-  name         = "Ubuntu Bionic"
-}
-
-resource "scaleway_server" "terraform" {
-  name  = "terraform"
-  image = "${data.scaleway_image.terraform.id}"
-  type  = "START1-S"
+  name = "Ubuntu Bionic"
 }
 resource "scaleway_ip" "ip" {
-  server = "${scaleway_server.terraform.id}"
+  server = "${scaleway_server.wave_tf.id}"
 }
 
+resource "scaleway_server" "wave_tf" {
+  name = "wave_tf"
+  image = "${data.scaleway_image.wave_tf.id}"
+  type = "START1-S"
+}
 
-resource "scaleway_volume" "terraform" {
-  name       = "terraformvolume"
+resource "scaleway_volume" "wave_tf" {
+  name = "wave_tf"
   size_in_gb = 50
-  type       = "l_ssd"
+  type = "l_ssd"
 }
---
+++ var.tf
@@ -0,0 +1,5 @@
+variable "token" {}
+variable "organization" {}
+variable "region" {
+  default = "ams1"
+}
